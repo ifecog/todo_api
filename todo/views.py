ea9e6313d2a8f19cd0d2297dcabe7d14de7c2bb8
progIfe <ifeoluwasamson90@gmail.com>
2023-02-15 17:12:15 +0100

    Form Class and View Home Function Code Writing

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Todo
+from .forms import TodoForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'pages/home.html')
