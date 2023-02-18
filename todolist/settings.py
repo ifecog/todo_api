e2899b1124d695259dfa9457250f6e8b323f2284
progIfe <ifeoluwasamson90@gmail.com>
2023-02-18 08:02:15 +0100

    REST Api Setup and Configuration, api app setup

--- todolist/settings.py
+++ todolist/settings.py
@@ -33,12 +33,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todo.apps.TodoConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- todolist/urls.py
+++ todolist/urls.py
@@ -21,4 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+    # api
+    path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
